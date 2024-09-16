a46b8be7b1e2b0cb4218737ad993b76d9148218a
Hitesh Soni <sonihitesh2024@gmail.com>
2024-09-17 02:59:14 +0530

    SUM OF NUMBERS

--- SumofNnumbers.cpp
+++ SumofNnumbers.cpp
@@ -9,7 +9,29 @@ int main(){
         sum=sum+i;
     }
     cout<<"The sum of n numbers is : "<<sum;
-    return 0;
+//Sum of n odd numbers
+// for(int i=1;i<=n;i=i+2){
+// sum =sum +i;
+// }
+// cout<<"the sum of n odd numbers is :"<<sum;
+/*another way to print sum of odd numbers
+for(int i=0;i<=n;i++){
+   if(i%2 !=0){
+    sum=sum+i;
+   }
+}
+cout<<sum;
 
+WAY TO PRINT EVEN SUM 
+for(int i=0;i<=n;i++){
+   if(i%2 =0){
+    sum=sum+i;
+   }
+}
+cout<<sum;
+*/
+
+
+    return 0;
 
 }
