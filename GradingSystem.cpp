b53e7de9eca1664fbd85793309a3bdbad44bf949
Hitesh Soni <sonihitesh2024@gmail.com>
2024-10-11 05:32:34 +0530

    date: 11 oct 2024

---
+++ GradingSystem.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int marks;
+    cout<<"Enter your marks: ";
+    cin>>marks;
+    if(marks>100){
+        cout<<"Enter your marks out of 100";
+    }else if(marks==100){
+        cout<<"you got 100/100 Grade: A";
+    } else if(marks>=90 && marks <=99){
+        cout<<"Grade: A";
+    }else if(marks>=75 && marks<90){
+        cout<<"Grade: B";
+    }else if(marks>=45 && marks<75){
+        cout<<"Grade: C";
+    }else if(marks<0){
+        cout<<"Enter valid marks";
+    }else{
+        cout<<"You are Failed";
+    }
+    return 0;
+}
