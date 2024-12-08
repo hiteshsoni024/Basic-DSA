95f63abcf4429c4b193aa6ed15caefaaa41391df
hiteshsoni024 <sonihitesh2024@gmail.com>
2024-12-08 20:47:32 +0530

    8dec

---
+++ 03_operators.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+int main(){
+//arithmetic operator
+int a;
+int b;
+int add,sub,multiple,division,remender;
+cout<<"enter (integer) a:";
+cin>>a;
+cout<<"enter (integer) b:";
+cin>>b;
+//add
+add=a+b;
+cout<<"add is:"<<add<<endl;
+//substract
+sub=a-b;
+cout<<"sub is :"<<sub<<endl;
+//multiplication
+multiple=a*b;
+cout<<"multiply is:"<<multiple<<endl;
+       
+//devision
+division=a/b;
+cout<<"division is:"<<division<<endl;
+        
+//remainder
+remender=a%b;
+cout<<"remender is:"<<remender<<endl;
+cout<<(3>5)<<endl;//false -> 0
+cout<<(3<5)<<endl;//true -> 1
+       
+
+    //relational operator
+    
+
+
+
+    //logical operator
+
+    return 0;
+} 
