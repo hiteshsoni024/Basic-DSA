35855ff186414e8d5c6d7ac7d18c2e9acb3be9ff
hiteshsoni024 <sonihitesh2024@gmail.com>
2025-01-14 18:18:28 +0530

    tuesday 14 jan dsa

--- 26_pattern.cpp
+++ 26_pattern.cpp
@@ -1,40 +1,85 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    int n;
-    cout << "The number of rows: ";
-    cin >> n;
-    //top
-    for ( int i =0 ; i<n ; i++){
-        //space 
-        for (int j =0 ; j<n-i-1 ; j++){
-            cout << " ";
-        } 
 
-        cout<< "*";
-
-        if(i != 0 ){
-        //space
-            for(int j =0; j<2*i-1;j++){
-                cout<< " ";
+int main() {
+    int n; // Height of the diamond
+    cout<<"Enter the minimum height of diamond:";
+    cin>>n;
+    //checking the n if it is even then decrease it by 1 and that will be the height of diamond 
+    if (n%2==0){
+        n=n-1;
+        cout<<"Your Entered number is even that's why we redused the height of diamond by 1"<<endl;
+        // Upper half of the diamond
+    for (int i = 1; i <= n; i += 2) {
+        for (int j = 1; j <= n - i; j += 2) {
+            cout << "  ";
+        }
+        for (int j = 1; j <= i; j++) {
+            if (j == 1 || j == i) {
+                cout << "* ";
+            } else {
+                cout << "  ";
             }
-         cout<< "*";
         }
-        cout<<endl;
+        cout << endl;
     }
-    //bottom
-    for (int i=0; i<n-1; i++){
-        //space
-        for(int j =0; j<i+1;j++){
-            cout << " ";
-        } 
-        if( i != n-1 ){
-        for (int j=0; j<2*(n-i)-5; j++){
-            cout << " ";
+
+    // Lower half of the diamond
+    for (int i = n - 2; i >= 1; i -= 2) {
+        for (int j = 1; j <= n - i; j += 2) {
+            cout << "  ";
         }
-        cout<<"*";
+        for (int j = 1; j <= i; j++) {
+            if (j == 1 || j == i) {
+                cout << "* ";
+            } else {
+                cout << "  ";
+            }
         }
-        cout<<endl;
+        cout << endl;
     }
+
+    }else{
+        // Upper half of the diamond
+    for (int i = 1; i <= n; i += 2) {
+        for (int j = 1; j <= n - i; j += 2) {
+            cout << "  ";
+        }
+        for (int j = 1; j <= i; j++) {
+            if (j == 1 || j == i) {
+                cout << "* ";
+            } else {
+                cout << "  ";
+            }
+        }
+        cout << endl;
+    }
+
+    // Lower half of the diamond
+    for (int i = n - 2; i >= 1; i -= 2) {
+        for (int j = 1; j <= n - i; j += 2) {
+            cout << "  ";
+        }
+        for (int j = 1; j <= i; j++) {
+            if (j == 1 || j == i) {
+                cout << "* ";
+            } else {
+                cout << "  ";
+            }
+        }
+        cout << endl;
+    }
+    }
+
+    
+
     return 0;
 }
+//hollow diamond
+//    *
+//   * *
+//  *   *
+// *     *
+//  *   *
+//   * *
+//    *
