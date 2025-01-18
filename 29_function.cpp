338dc678ece3cc517988c10625dee4719078d201
hiteshsoni024 <sonihitesh2024@gmail.com>
2025-01-18 17:55:05 +0530

    changes

--- 29_function.cpp
+++ 29_function.cpp
@@ -1,6 +1,7 @@
 //sum of n numbers using function
 #include<iostream>
 using namespace std;
+
 int sum(int n){
     int sum =0;
     for(int i=0;i<=n;i++){
--- 36_DecitoBinary.cpp
+++ 36_DecitoBinary.cpp
@@ -1,6 +1,8 @@
 //Decimal to binary
 #include<iostream>
 using namespace std;
+
+//conversion of decimal to binary
 int DecToBin(int deciNum){
     int ans=0;
     int power =1; //10^0 -> 10^1 -> 10^2
@@ -13,12 +15,35 @@ int DecToBin(int deciNum){
     }
     return ans;
 }
+//conversion odf binary to decimal
+int BinTODeci(int binNum){
+    int ans=0;
+    int power =1; 
+
+    while(binNum>0){
+        int rem = binNum % 10;
+        binNum = binNum /10;
+        ans = ans + (rem * power);
+        power= power * 2;
+    }
+    return ans;
+
+}
 int main(){
+    int choice ;
+    cout<< "what you want to do :"<<endl<<" press 1 : Decimal to binary conversion"<<endl<<"press 2 : Binary to decimal conversion"<<endl;
+    cin>>choice;
+    if (choice==1){
     int deciNum;
     cout<<"enter the Decimal number: ";
     cin>>deciNum;
-
     cout<<DecToBin(deciNum);
+    }else{
+        int BinNum ;
+        cout<<"enter a binary number: ";
+        cin>>BinNum;
+        cout<<BinTODeci(BinNum);
+    }
     return 0;
 
 }
